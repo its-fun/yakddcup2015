0d8ebf45f905a7d6902caa37a3efd05d0214e6c2
hsinhuang <leon.hxin@gmail.com>
2015-06-30 21:10:32 +0800

    fix an issue than NA cause

--- Util.py
+++ Util.py
@@ -3,12 +3,13 @@
 
 
 import numpy as np
+import pandas as pd
 
 
 def dataframe_checker(logger):
     def check(df, msg):
         nr, nc = df.shape
         logger.debug('%s: shape: %d x %d, has_nan: %s', msg, nr, nc - 1,
-                     np.any(np.isnan(df)))
+                     np.any(pd.isnull(df)))
         logger.debug('%s: columns: %s', msg, df.columns.values)
     return check
