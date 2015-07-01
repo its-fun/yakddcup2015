4053400a041d8b7388bd19ca9d77f611d639aa37
hsinhuang <leon.hxin@gmail.com>
2015-07-01 21:05:54 +0800

    pass

--- dropouts.py
+++ dropouts.py
@@ -3,14 +3,14 @@
 
 
 """
-Total: 11
+Total: 12
 
-0~1: 课程最后十天有操作的人数，与选课人数的比例
+0~2: 课程最后十天有操作的人数，与选课人数的比例: 课程有更新的最后十天、课程有操作的最后十天
 
-2~5: 用户平均每个课程Dropout的次数，在该课程中Dropout的次数，与平均每个课程Dropout次数的比例；
+3~6: 用户平均每个课程Dropout的次数，在该课程中Dropout的次数，与平均每个课程Dropout次数的比例；
 总Dropout持续时长与课程持续时间的比例
 
-6~10: 用户在所有课程上Dropout总持续时长与课程持续时间的比例的：平均值、方差、最大值、最小值；
+7~11: 用户在所有课程上Dropout总持续时长与课程持续时间的比例的：平均值、方差、最大值、最小值；
 有Dropout行为的课程占用户所选课程总数的比例
 """
 
@@ -58,23 +58,39 @@ def extract(enrollment, base_date):
         .reset_index().rename(columns={'username': 'user_count'})
 
     import time_related
-    log_all = pd.merge(log_all, time_related.course_duration(), how='left',
-                       on='enrollment_id')
-    remain_log = log_all[log_all['time'] > log_all['et'] - timedelta(days=10)]
-    remain_count_of_course = pd.merge(
-        remain_log, enroll_all, how='left', on='enrollment_id')\
-        .groupby('course_id').agg({'username': lambda us: len(np.unique(us))})\
-        .reset_index().rename(columns={'username': 'remain_count'})
+    course_t1 = time_related.course_duration(log_all, obj_all, enroll_all)
+    course_t2 = pd.merge(log_all, enroll_all, how='left', on='enrollment_id')\
+        .groupby('course_id').agg({'time': [np.min, np.max]})
+    course_t2.columns = ['st2', 'et2']
+    course_t2.reset_index(inplace=True)
 
-    D_last = pd.merge(user_count_of_course, remain_count_of_course, how='left',
-                      on='course_id')
-    D_last['remain_count'] = D_last['remain_count'] / D_last['user_count']
+    log_all_t = pd.merge(log_all, course_t1, how='left', on='course_id')
+    log_all_t = pd.merge(log_all_t, course_t2, how='left', on='course_id')
 
-    # 0~1: 课程最后十天有操作的人数，与选课人数的比例
-    logger.debug('0~1')
+    remain_log1 = log_all_t[
+        log_all_t['time'] > log_all_t['et'] - timedelta(days=10)]
+    remain_count_of_course1 = remain_log1.groupby('course_id')\
+        .agg({'username': lambda us: len(np.unique(us))})\
+        .reset_index().rename(columns={'username': 'remain_count1'})
+
+    remain_log2 = log_all_t[
+        log_all_t['time'] > log_all_t['et2'] - timedelta(days=10)]
+    remain_count_of_course2 = remain_log2.groupby('course_id')\
+        .agg({'username': lambda us: len(np.unique(us))})\
+        .reset_index().rename(columns={'username': 'remain_count2'})
+
+    D_last = pd.merge(user_count_of_course, remain_count_of_course1,
+                      how='left', on='course_id')
+    D_last = pd.merge(D_last, remain_count_of_course2,
+                      how='left', on='course_id')
+    D_last.fillna(0, inplace=True)
+    D_last['remain_count1'] = D_last['remain_count1'] / D_last['user_count']
+    D_last['remain_count2'] = D_last['remain_count2'] / D_last['user_count']
+
+    # 0~2: 课程最后十天有操作的人数，与选课人数的比例: 课程有更新的最后十天、课程有操作的最后十天
+    logger.debug('0~2')
 
     def __get_dropout_count__(group):
-        last_op_t = group['time'].max()
         course_et = group['et'].max()
         course_st = group['st'].min()
         group_t = group['time'].sort(inplace=False)
@@ -82,10 +98,7 @@ def extract(enrollment, base_date):
                       ['time'].dt.days)
         dc = np.sum(dd >= 10)
         dd_sum = np.sum(dd)
-        if (course_et - last_op_t).days >= 10:
-            dc += 1
-            dd_sum += (course_et - last_op_t).days
-        return pd.Series([dc, dd_sum, dd_sum / (course_et - course_st).days],
+        return pd.Series([dc, dd_sum / (course_et - course_st).days],
                          index=['dropout_count', 'dt_ratio'])
     D_count = log_all.groupby('enrollment_id').apply(__get_dropout_count__)\
         .reset_index()
@@ -97,13 +110,15 @@ def extract(enrollment, base_date):
     D_count = pd.merge(D_count, avg_count, how='left', on='username')
     D_count['dc_ratio'] = D_count['dropout_count'] / D_count['avg_dc']
 
-    # 2~5: 用户平均每个课程Dropout的次数，在该课程中Dropout的次数，
+    # 3~6: 用户平均每个课程Dropout的次数，在该课程中Dropout的次数，
     # 与平均每个课程Dropout次数的比例；总Dropout持续时长与课程持续时间的比例
     X1 = D_count.copy()
     del X1['username']
     del X1['course_id']
 
-    # 6~10: 用户在所有课程上Dropout总持续时长与课程持续时间的比例的：
+    logger.debug('3~6')
+
+    # 7~11: 用户在所有课程上Dropout总持续时长与课程持续时间的比例的：
     # 平均值、方差、最大值、最小值；有Dropout行为的课程占用户所选课程总数的比例
     X2 = D_count.groupby('username')\
         .agg({'dt_ratio': [np.average, np.std, np.max, np.min]}).reset_index()
@@ -113,9 +128,11 @@ def extract(enrollment, base_date):
     import events
     UC_count = events.count_courses_by_user(log_all, enroll_all)
     X3 = pd.merge(DC_count, UC_count, how='left', on='username')
-    X3['drop_course_ratio'] = (X3['d_course'] / X3['course_count'])
+    X3['drop_course_ratio'] = X3['d_course'] / X3['course_count']
     del X3['d_course']
     del X3['course_count']
+
+    logger.debug('7~11')
 
     check_dataframe = Util.dataframe_checker(logger)
 
