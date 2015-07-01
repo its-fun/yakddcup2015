65d183a116f9ffcfc66d2c34a78274d186d6db47
hsinhuang <leon.hxin@gmail.com>
2015-07-01 20:06:19 +0800

    add log for df with Na

--- Util.py
+++ Util.py
@@ -9,7 +9,11 @@ import pandas as pd
 def dataframe_checker(logger):
     def check(df, msg):
         nr, nc = df.shape
+        logger.debug('%s: columns: %s', msg, df.columns.values)
         logger.debug('%s: shape: %d x %d, has_nan: %s', msg, nr, nc - 1,
                      np.any(pd.isnull(df)))
-        logger.debug('%s: columns: %s', msg, df.columns.values)
+        if np.any(pd.isnull(df)):
+            for c in df.columns.values:
+                if np.any(pd.isnull(df[c])):
+                    logger.debug('%s: column `%s` has Na', msg, c)
     return check
