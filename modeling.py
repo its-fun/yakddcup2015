378c380428ed925a770b9b5824ba73cdc558f8d6
hsinhuang <leon.hxin@gmail.com>
2015-07-05 14:58:18 +0800

    modeling/erf: adjust param

--- modeling.py
+++ modeling.py
@@ -410,7 +410,7 @@ def erf():
     del X
 
     rf = ExtraTreesClassifier(n_estimators=15000, oob_score=True, n_jobs=-1,
-                              class_weight='auto')
+                              class_weight='auto', bootstrap=True)
     rf.fit(X_scaled, y)
 
     logger.debug('ExtraTreesClassifier fitted')
